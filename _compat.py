c1786b7146b427fa29f0f1898feec202beb4b681
Varshitha-2423 <varshitha2447@gmail.com>
2026-09-09 13:36:27 +0530

    Add files via upload

--- _compat.py
+++ _compat.py
@@ -1,94 +1,56 @@
-# SPDX-License-Identifier: MIT
-
-import inspect
 import platform
 import sys
-import threading
 
-from collections.abc import Mapping, Sequence  # noqa: F401
-from typing import _GenericAlias
+__all__ = ['install', 'NullFinder']
 
 
-PYPY = platform.python_implementation() == "PyPy"
-PY_3_9_PLUS = sys.version_info[:2] >= (3, 9)
-PY_3_10_PLUS = sys.version_info[:2] >= (3, 10)
-PY_3_11_PLUS = sys.version_info[:2] >= (3, 11)
-PY_3_12_PLUS = sys.version_info[:2] >= (3, 12)
-PY_3_13_PLUS = sys.version_info[:2] >= (3, 13)
-PY_3_14_PLUS = sys.version_info[:2] >= (3, 14)
-
-
-if PY_3_14_PLUS:  # pragma: no cover
-    import annotationlib
-
-    _get_annotations = annotationlib.get_annotations
-
-else:
-
-    def _get_annotations(cls):
-        """
-        Get annotations for *cls*.
-        """
-        return cls.__dict__.get("__annotations__", {})
-
-
-class _AnnotationExtractor:
+def install(cls):
     """
-    Extract type annotations from a callable, returning None whenever there
-    is none.
+    Class decorator for installation on sys.meta_path.
+
+    Adds the backport DistributionFinder to sys.meta_path and
+    attempts to disable the finder functionality of the stdlib
+    DistributionFinder.
+    """
+    sys.meta_path.append(cls())
+    disable_stdlib_finder()
+    return cls
+
+
+def disable_stdlib_finder():
+    """
+    Give the backport primacy for discovering path-based distributions
+    by monkey-patching the stdlib O_O.
+
+    See #91 for more background for rationale on this sketchy
+    behavior.
     """
 
-    __slots__ = ["sig"]
+    def matches(finder):
+        return getattr(
+            finder, '__module__', None
+        ) == '_frozen_importlib_external' and hasattr(finder, 'find_distributions')
 
-    def __init__(self, callable):
-        try:
-            self.sig = inspect.signature(callable)
-        except (ValueError, TypeError):  # inspect failed
-            self.sig = None
+    for finder in filter(matches, sys.meta_path):  # pragma: nocover
+        del finder.find_distributions
 
-    def get_first_param_type(self):
-        """
-        Return the type annotation of the first argument if it's not empty.
-        """
-        if not self.sig:
-            return None
 
-        params = list(self.sig.parameters.values())
-        if params and params[0].annotation is not inspect.Parameter.empty:
-            return params[0].annotation
+class NullFinder:
+    """
+    A "Finder" (aka "MetaPathFinder") that never finds any modules,
+    but may find distributions.
+    """
 
-        return None
-
-    def get_return_type(self):
-        """
-        Return the return type if it's not empty.
-        """
-        if (
-            self.sig
-            and self.sig.return_annotation is not inspect.Signature.empty
-        ):
-            return self.sig.return_annotation
-
+    @staticmethod
+    def find_spec(*args, **kwargs):
         return None
 
 
-# Thread-local global to track attrs instances which are already being repr'd.
-# This is needed because there is no other (thread-safe) way to pass info
-# about the instances that are already being repr'd through the call stack
-# in order to ensure we don't perform infinite recursion.
-#
-# For instance, if an instance contains a dict which contains that instance,
-# we need to know that we're already repr'ing the outside instance from within
-# the dict's repr() call.
-#
-# This lives here rather than in _make.py so that the functions in _make.py
-# don't have a direct reference to the thread-local in their globals dict.
-# If they have such a reference, it breaks cloudpickle.
-repr_context = threading.local()
+def pypy_partial(val):
+    """
+    Adjust for variable stacklevel on partial under PyPy.
 
-
-def get_generic_base(cl):
-    """If this is a generic class (A[str]), return the generic base for it."""
-    if cl.__class__ is _GenericAlias:
-        return cl.__origin__
-    return None
+    Workaround for #327.
+    """
+    is_pypy = platform.python_implementation() == 'PyPy'
+    return val + is_pypy
